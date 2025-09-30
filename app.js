cb3d4e580b1dfe15c3c4807fffa8ce48968e7773
HyoketsuSenpai <Youseftela@gmail.com>
2025-09-30 13:21:39 +0300

    small fix on the middleware

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ var app = express();
 
 // logging middleware
 app.use(function(req,res,next){
-        console.log("In comes a requesr to: " + request.url);
+        console.log("In comes a requesr to: " + req.url);
         next();
 });
+
+
 
 app.use(function(request, response){
         response.writeHead(200, {"Content-Type": "text/plain"} )
         response.end("Hello World");
 });
 
-// http.createServer(app).listen(3000);
-app.listen(3000);
+http.createServer(app).listen(3000);
+// app.listen(3000);
+console.log("server is running in http://localhost:3000");
 
 // 57
 
