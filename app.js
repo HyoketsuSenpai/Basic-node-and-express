050cd3c04017dbaa9978929c2989f9cdfe9333c6
HyoketsuSenpai <Youseftela@gmail.com>
2025-09-30 14:14:48 +0300

    add a fake auth middleware

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ var app = express();
 
 // logging middleware
 app.use(function(req,res,next){
-        console.log("In comes a requesr to: " + req.url);
+        console.log("In comes a request to: " + req.url);
         next();
 });
 
-
+// fake auth middleware
+app.use(function(req,res,next){
+        var mins = (new Date()).getMinutes();
+        if(mins % 2 === 0){
+               return next();
+        }
+        
+        res.statusCode = 403;
+        res.end('Not Authorized');
+        
+});
 
 app.use(function(request, response){
-        response.writeHead(200, {"Content-Type": "text/plain"} )
-        response.end("Hello World");
+        response.end('Secret info: the password is "swordfish"!');
 });
 
 http.createServer(app).listen(3000);
